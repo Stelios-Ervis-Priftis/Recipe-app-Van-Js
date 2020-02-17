bf272911de1d4e28d2162240844fa85f00036049
Stelios-Ervis-Priftis <steliospriftis@gmail.com>
2020-02-17 16:03:56 +0200

    Setup recipes.js to loadRecipes/saveRecipes/getRecipes/createRecipe/removeRecipe/toggleRecipe

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 import { log, doc } from './helpers'
+import { createRecipe, getRecipes, removeRecipe, toggleRecipe } from './recipes'
 
 log('Load index.js')
+
+// createRecipe()
+// log(getRecipes())
+// removeRecipe()
+toggleRecipe('9e827836-c154-44e2-9085-b287c5bd7d82')
+log(getRecipes())
---
+++ src/recipes.js
@@ -0,0 +1,84 @@
+import moment from 'moment'
+import uuidv4 from'uuid/v4'
+import { log } from './helpers'
+
+// Setup the empty recipes array
+let recipes = []
+
+// loadRecipes
+const loadRecipes = () => {
+    const recipesJson = localStorage.getItem('recipes')
+
+    try {
+        recipes = recipesJson ? JSON.parse(recipesJson) : []
+    } catch (error) {
+        recipes = []
+    }
+}
+// Arguments: none
+// Return value: none
+
+// saveRecipes
+const saveRecipes = () => {
+    localStorage.setItem('recipes', JSON.stringify(recipes))
+}
+// Arguments: none
+// Return value: none
+
+// getRecipes
+const getRecipes = () => recipes
+// Arguments: none
+// Return value: todos array
+
+// createRecipe
+const createRecipe = () => {
+    const id = uuidv4()
+    const timestamp = moment().valueOf()
+    
+    recipes.push({
+        id: id,
+        title: 'Recipe title',
+        body: 'Steps',
+        createdAt: timestamp,
+        upDateAt: timestamp,
+        ingredients: {
+            text: 'ingredients',
+            completed: false
+        }
+    })
+    saveRecipes()
+}
+// Arguments: todo text
+// Return value: none
+
+// removeRecipe
+const removeRecipe = (id) => {
+    const recipeIndex = recipes.findIndex((recipe) => recipe.id === id)
+    log(recipeIndex)
+
+    if (recipeIndex > -1) {
+        recipes.splice(recipeIndex, 1)
+        saveRecipes()
+    }
+}
+// Arguments: id of todo to remove
+// Return value: none
+
+// toggleRecipe
+const toggleRecipe = (id) => {
+    const recipe = recipes.find((recipe) => {
+        return recipe.id === id
+    })
+    log(recipe)
+
+    if (recipe) {
+        recipe.ingredients.completed = !recipe.ingredients.completed
+        saveRecipes()
+    }
+}
+// Arguments: id of todo to toggle
+// Return value: none
+
+loadRecipes()
+// Make sure to call loadRecipe and setup the exports
+export { createRecipe, getRecipes, removeRecipe, toggleRecipe }
