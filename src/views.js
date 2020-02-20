f10e78b50014d101cd77361c16557052cde6ab8d
Stelios-Ervis-Priftis <steliospriftis@gmail.com>
2020-02-20 14:16:03 +0200

    Created generateSummaryDom for feature use to generate message under recipes that indicates if the recipe contains all the ingredients plus created input tag for searching the recipe and button tag for create the recipe

--- src/views.js
+++ src/views.js
@@ -1,11 +1,12 @@
 import { log, doc } from './helpers'
 import { getRecipes } from './recipes'
 
+// generateRecipeDOM
+// Get the DOM elements for an individual recipe
 const generateRecipeDom = (recipe) => {
     const recipeRoot = doc.createElement('div')
     const recipeEl = doc.createElement('a')
     const textEl = doc.createElement('p')
-    const statusEl = doc.createElement('p')
 
     if (recipe.title.length ) {
         textEl.textContent = recipe.title
@@ -13,15 +14,18 @@ const generateRecipeDom = (recipe) => {
         textEl.textContent = 'Unnamed Recipe'
     }
     recipeEl.setAttribute('href', `/edit.html#${recipe.id}`)
-    statusEl.textContent = `Structure if all the ingredients exit`
 
     recipeEl.appendChild(textEl)
-    recipeEl.appendChild(statusEl)
+    recipeEl.appendChild(generateSummaryDom())
     recipeRoot.appendChild(recipeEl)
 
     return recipeRoot
 }
+// Arguments: recipe
+// Return value: the recipe element
 
+// renderRecipes
+// Render application todos base on the filters
 const renderRecipes = () => {
     const recipesEl = doc.querySelector('#recipes')
     const recipes = getRecipes()
@@ -31,5 +35,16 @@ const renderRecipes = () => {
         recipesEl.appendChild(recipeTitle)
     })
 }
+// Arguments: none
+// Return value: none
 
-export { generateRecipeDom, renderRecipes }
+
+const generateSummaryDom = () => {
+    const statusEl = doc.createElement('p')
+    statusEl.textContent = `Structure if all the ingredients exit`
+    return statusEl
+}
+// Arguments: incompletedTodos
+// Return value: the summary
+
+export { generateRecipeDom, renderRecipes, generateSummaryDom }
