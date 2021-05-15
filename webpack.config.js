cbb92eaab52873a9ed944c66b659b06a508f234a
Stelios-Ervis-Priftis <steliospriftis@gmail.com>
2021-05-15 16:55:43 +0200

    Styling reparings on media queries

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require("path")
+const path = require('path')
 
 module.exports = {
-  entry: {
-    index: ["@babel/polyfill", "./src/index.js"],
-    edit: ["@babel/polyfill", "./src/edit.js"]
-  },
-  output: {
-    path: path.resolve(__dirname, "public/dist"),
-    filename: "[name]-bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  },
-  devServer: {
-      contentBase: path.resolve(__dirname, "public"),
-      watchContentBase: true,
-      publicPath:"/dist/"
-  },
-  devtool: "source-map"
+	entry: {
+		index: ['@babel/polyfill', './src/index.js'],
+		edit: ['@babel/polyfill', './src/edit.js'],
+	},
+	output: {
+		path: path.resolve(__dirname, 'public/dist'),
+		filename: '[name]-bundle.js',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env'],
+					},
+				},
+			},
+		],
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, 'public'),
+		watchContentBase: true,
+		publicPath: '/dist/',
+	},
+	devtool: 'source-map',
 }
